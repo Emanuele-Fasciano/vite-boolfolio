9f04742cb6130e62158ecb5b1b68d5464b16e327
Emanuele-Fasciano <emanuelefasciano@gmail.com>
2023-04-21 19:26:41 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+// importo il componente 
+import AppHeader from './components/AppHeader.vue'
+
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+
+        // lista dei componenti da utilizzare nell' app
+        components: {
+        AppHeader
+    }
+}
+</script>
+
+
+<template>
+    <!-- utilizzare il componente sottoforma di tag -->
+    <AppHeader />
+</template>
+
+<style lang="scss" scoped></style>
